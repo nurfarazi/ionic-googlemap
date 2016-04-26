eac37631a3975ad4a305a3013b106ef39abed73e
nur.farazi <nur369188@gmail.com>
2016-04-26 10:16:51 +0600

    fix

--- www/js/app.js
+++ www/js/app.js
@@ -25,27 +25,35 @@ var mapapp = angular.module('starter', ['ionic'])
 
 mapapp.controller("mapCtrl", function ($scope) {
 
-    google.maps.event.addDomListener(window, 'load', function () {
-        var myLatlng = new google.maps.LatLng(37.3000, -120.4833);
+    console.log("Not initialize");
+    google.maps.event.addDomListener(window, 'load', initialize());
 
-        var mapOptions = {
-            center: myLatlng,
-            zoom: 16,
-            mapTypeId: google.maps.MapTypeId.ROADMAP
-        };
+    function initialize() {
+        google.maps.event.addDomListener(window, 'load', function () {
+            var myLatlng = new google.maps.LatLng(37.3000, -120.4833);
 
-        var map = new google.maps.Map(document.getElementById("map"), mapOptions);
+            var mapOptions = {
+                center: myLatlng,
+                zoom: 16,
+                mapTypeId: google.maps.MapTypeId.ROADMAP
+            };
 
-        navigator.geolocation.getCurrentPosition(function (pos) {
-            map.setCenter(new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude));
-            var myLocation = new google.maps.Marker({
-                position: new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude),
-                map: map,
-                title: "My Location"
+            var map = new google.maps.Map(document.getElementById("map"), mapOptions);
+
+            navigator.geolocation.getCurrentPosition(function (pos) {
+                map.setCenter(new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude));
+                var myLocation = new google.maps.Marker({
+                    position: new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude),
+                    map: map,
+                    title: "My Location"
+                });
             });
-        });
 
-        $scope.map = map;
-    });
+            $scope.map = map;
+        });
+    };
+
+
+
 
 });
